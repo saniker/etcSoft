c08f31d866f5e12d03de25494896113e622475fc
infostore <korman21@gmail.com>
2012-11-19 22:28:17 +0900

    frame animation 적용

--- appSite/gen/com/owl/appsite/R.java
+++ appSite/gen/com/owl/appsite/R.java
@@ -16,14 +16,20 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int app_mark=0x7f020000;
-        public static final int bg=0x7f020001;
-        public static final int ic_action_search=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
+        public static final int anim_list=0x7f020000;
+        public static final int app_mark=0x7f020001;
+        public static final int bg=0x7f020002;
+        public static final int ic_action_search=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int menu1=0x7f020005;
+        public static final int menu2=0x7f020006;
+        public static final int menu3=0x7f020007;
+        public static final int menu4=0x7f020008;
     }
     public static final class id {
+        public static final int anim_bg=0x7f080001;
         public static final int img_app_mark=0x7f080000;
-        public static final int menu_settings=0x7f080001;
+        public static final int menu_settings=0x7f080002;
     }
     public static final class layout {
         public static final int activity_app_site=0x7f030000;
--- appSite/src/com/owl/appsite/AppSite.java
+++ appSite/src/com/owl/appsite/AppSite.java
@@ -1,7 +1,11 @@
 package com.owl.appsite;
 
-import android.os.Bundle;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -19,11 +23,56 @@ public class AppSite extends Activity {
         ImageView image = (ImageView) findViewById(R.id.img_app_mark);
         Animation alphaAnim = AnimationUtils.loadAnimation(this, R.anim.tween);
         image.startAnimation(alphaAnim);
+
+        // frame animation
+
+//        ImageView img = (ImageView) findViewById(R.id.anim_bg);
+//        img.setBackgroundResource(R.drawable.anim_list);
+//
+//        AnimationDrawable fromAnimation = (AnimationDrawable) img.getBackground();
+//        fromAnimation.start();
+
+        // 위 코드와 아래 코드 차이점이 없는데?
+        // 위에 코드가 더 좋은듯...
+
+        ImageView img = (ImageView) findViewById(R.id.anim_bg);
+        img.setBackgroundResource(R.drawable.anim_list);
+
+        SlideStart slideStart = new SlideStart();
+        SlideEnd slideEnd = new SlideEnd();
+
+        Timer startTimer = new Timer(false);
+        startTimer.schedule(slideStart, 1000);
+        // 1초마다 실행되면 fromAnimation.start(); 이 부분이
+        // 반복 호출되는데, 이상이 없나?
+
+        Timer endTimer = new Timer(false);
+        endTimer.schedule(slideEnd, 10000);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_app_site, menu);
         return true;
+    }
+
+    class SlideStart extends TimerTask {
+
+        @Override
+        public void run() {
+            ImageView img = (ImageView) findViewById(R.id.anim_bg);
+            AnimationDrawable fromAnimation = (AnimationDrawable) img.getBackground();
+            fromAnimation.start();
+        }
+    }
+
+    class SlideEnd extends TimerTask {
+
+        @Override
+        public void run() {
+            ImageView img = (ImageView) findViewById(R.id.anim_bg);
+            AnimationDrawable fromAnimation = (AnimationDrawable) img.getBackground();
+            fromAnimation.stop();
+        }
     }
 }
